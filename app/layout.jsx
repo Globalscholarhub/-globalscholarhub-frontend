d9d87c4e571ee1efe494b27a85d4aa70f56d5f95
Globalscholarhub <visa.tripmaker@gmail.com>
2025-11-22 00:37:46 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,9 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-100 text-gray-900">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
